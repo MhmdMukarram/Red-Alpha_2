03a88090973aa44cb2138e84782739505c29a772
Red Alpha <93113261+MhmdMukarram@users.noreply.github.com>
2022-01-21 21:28:14 +0530

    Update audiocmds.js

--- plugins/audiocmds.js
+++ plugins/audiocmds.js
@@ -7,7 +7,7 @@ const Config = require('../config');
 
         
         const cmd = "```Ayin```\n```Ayinu```\n```Bgm```\n```Bot```\n```Bye```\n```Good night```\n```Hello```\n```Hi```\n```Mukarram```\n```Neymar```\n```Pm```\n```RedAlpha```\n```Sed```\n```alive```\n```assist```\n```ban```\n```bgm```\n```bot```\n```converting```\n```fake```\n```fork```\n```fuck```\n```music```\n```myre```\n```njan```\n```number```\n```oombi```\n```owner```\n```poda```\n```remove```\n```reply```\n```sed```\n```subscribe```\n```xxxtentation```";
-        const {data} = await axios(`https://gist.github.com/souravkl11/f9a683c137a67af4133f62b60951f973`)
+        const {data} = await axios(`https://gist.github.com/MhmdMukarram/d68fdd4980fd0d08cb9f9741b6d0e082`)
         const { souravkl11 } = data
         await message.client.sendMessage(message.jid, cmd, MessageType.text, { quoted: message.data });
    });
@@ -17,7 +17,7 @@ if (Config.WORKTYPE == 'public') {
     skl.addCommand({pattern: 'audio', fromMe: false, desc: 'Gives audio commands' , dontAddCommandList: true }, async (message, match) => {
 
         const cmd = "```Ayin```\n```Ayinu```\n```Bgm```\n```Bot```\n```Bye```\n```Good night```\n```Hello```\n```Hi```\n```Mukarram```\n```Neymar```\n```Pm```\n```RedAlpha```\n```Sed```\n```alive```\n```assist```\n```ban```\n```bgm```\n```bot```\n```converting```\n```fake```\n```fork```\n```fuck```\n```music```\n```myre```\n```njan```\n```number```\n```oombi```\n```owner```\n```poda```\n```remove```\n```reply```\n```sed```\n```subscribe```\n```xxxtentation```";
-        const {data} = await axios('https://gist.github.com/souravkl11/f9a683c137a67af4133f62b60951f973')
+        const {data} = await axios('https://gist.github.com/MhmdMukarram/d68fdd4980fd0d08cb9f9741b6d0e082')
         const { souravkl11 } = data
         await message.client.sendMessage(message.jid, cmd, MessageType.text, { quoted: message.data });
    });
